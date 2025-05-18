2057ef6230849cb235fd71a70752bdd15c4d00ad
nguyenchanh0201 <522H0044@student.tdtu.edu.vn>
2025-05-19 01:02:28 +0700

    Setup project

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
