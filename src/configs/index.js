3a14fff4c1ceab7363325004c7a5d477c168c5c9
nguyenchanh0201 <522H0044@student.tdtu.edu.vn>
2025-05-30 02:11:18 +0700

    Database config

---
+++ src/configs/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+  port: process.env.PORT || 3000,
+  dbUrl: process.env.DB_URL,
+  awsRegion: process.env.AWS_REGION,
+  awsAccessKey: process.env.AWS_ACCESS_KEY,
+  awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  awsBucketName: process.env.AWS_BUCKET_NAME,
+};
