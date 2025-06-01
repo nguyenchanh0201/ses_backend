36e2d53bd5c66ff53798a93726a92f039619cf5e
nguyenchanh0201 <522H0044@student.tdtu.edu.vn>
2025-06-01 19:00:21 +0700

    Adding get-inboxes by userId

---
+++ src/routes/inboxRoutes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+const InboxController = require('../controllers/inboxController')
+
+router.get('/inbox/:userId', InboxController.getInboxes);
+
+module.exports = router;
